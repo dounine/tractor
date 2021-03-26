488aa8c59aa1a7cb63d2d26e2b96c4cb7ad00519
lake <102535481@qq.com>
2021-03-26 15:46:41 +0800

    add socket behavior

---
+++ src/main/scala/com/dounine/tractor/behaviors/socket/SocketBehavior.scala
@@ -0,0 +1,78 @@
+package com.dounine.tractor.behaviors.socket
+
+import akka.actor.typed.{ActorRef, Behavior}
+import akka.actor.typed.scaladsl.Behaviors
+import akka.cluster.sharding.typed.scaladsl.ClusterSharding
+import akka.stream.SystemMaterializer
+import akka.stream.scaladsl.Source
+import com.dounine.tractor.model.models.BaseSerializer
+import com.dounine.tractor.tools.json.ActorSerializerSuport
+import org.slf4j.LoggerFactory
+
+object SocketBehavior extends ActorSerializerSuport {
+  sealed trait Command extends BaseSerializer
+
+  final case class Login(
+      token: String,
+      client: Option[ActorRef[BaseSerializer]]
+  ) extends Command
+
+  final case class LoginOk()
+
+  final case object Shutdown extends Command
+
+  final case class StreamComplete() extends Command
+
+  final case class Config(
+      userId: String = "",
+      updownId: String = ""
+  ) extends BaseSerializer
+
+  final case class DataStore(
+      phone: Option[String],
+      client: Option[ActorRef[BaseSerializer]],
+      config: Config
+  ) extends BaseSerializer
+
+  private final val logger = LoggerFactory.getLogger(SocketBehavior.getClass)
+
+  def apply(): Behavior[BaseSerializer] =
+    Behaviors.setup { context =>
+      {
+        val cluster = ClusterSharding(context.system)
+        val materializer = SystemMaterializer(context.system).materializer
+        def login(data: DataStore): Behavior[BaseSerializer] =
+          Behaviors.receiveMessage {
+            case e @ Login(token, client) => {
+              logger.info(e.logJson)
+              login(
+                data.copy(
+                  client = client
+                )
+              )
+            }
+            case e @ Shutdown => {
+              logger.info(e.logJson)
+              Behaviors.stopped
+            }
+          }
+
+        def logined(data: DataStore): Behavior[BaseSerializer] =
+          Behaviors.receiveMessage {
+            case e @ Shutdown => {
+              logger.info(e.logJson)
+              Behaviors.stopped
+            }
+          }
+
+        login(
+          DataStore(
+            phone = Option.empty,
+            client = Option.empty,
+            config = Config()
+          )
+        )
+      }
+    }
+
+}
